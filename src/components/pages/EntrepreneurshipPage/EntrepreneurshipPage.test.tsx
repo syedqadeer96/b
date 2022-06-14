51a9fed322a06008303e92a2929bfe644d51d002
syedqadeer96 <syed.qadeer@zemosolabs.com>
2022-06-14 16:46:57 +0530

    new3

--- src/components/pages/EntrepreneurshipPage/EntrepreneurshipPage.test.tsx
+++ src/components/pages/EntrepreneurshipPage/EntrepreneurshipPage.test.tsx
@@ -8,6 +8,12 @@ import '@testing-library/jest-dom';
 test('renders Book Detail', () => {
   render(<EntrepreneurshipPage></EntrepreneurshipPage>);
   
-  const BannerElement = screen.getByText(' Add to lib');
-  expect(BannerElement).toBeInTheDocument();
+  const BannElement = screen.getByText(' Add to lib');
+  expect(BannElement).toBeInTheDocument();
 });
+test('renders Book ', () => {
+    render(<EntrepreneurshipPage></EntrepreneurshipPage>);
+    
+    const BanElement = screen.getByText('     Trending Blinks');
+    expect(BanElement).toBeInTheDocument();
+  });
